4157606131d31a4c5672b9e98e23b4f31c2fb7c1
Radosław Biesek <radoslaw.biesek@gmail.com>
2019-02-08 18:04:08 +0100

    Init branch

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,23 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const OptimizeJsPlugin = require('optimize-js-plugin');
+
+const plugins = [
+    new HtmlWebpackPlugin({
+        template: 'src/index.html',
+        filename: 'index.html',
+        inject: 'body'
+    })
+];
 
 module.exports = (env) => {
+    if (env === 'production') {
+        plugins.push(
+            new OptimizeJsPlugin({
+                sourceMap: false
+            })
+        );
+    }
     return {
         mode: env || 'production',
         entry: './src/index.js',
@@ -30,6 +47,7 @@ module.exports = (env) => {
                 }
 
             ]
-        }
+        },
+        plugins: plugins
     }
 }
